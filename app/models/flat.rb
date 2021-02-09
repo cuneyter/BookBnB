e9518799156fa07a294e7f3c929faba59935cdb7
Cuneyt Ergun <cuneytergun@gmail.com>
2021-02-09 21:24:01 +0000

    add validations and association

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,5 +1,6 @@
 class Flat < ApplicationRecord
-  # belongs_to :user
+  belongs_to :user
+  # has_many :bookings
 
   validates :address_line_1, :address_line_2, :postcode, :city, presence: true
   validates :price, :max_guest_number, presence: true, numericality: true
