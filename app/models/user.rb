b12eeffacef13b72924f3e2a9fb84ad7244aa3d6
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-02-09 21:31:02 +0000

    user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :flats
-  has_many :bookings
+  has_many :flats, through: :bookings, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 end
