3e975da52ff879c6e68aa3b02b35099ffe4c6ef0
guillaume grimbert <guillaumegrimbert@gmail.com>
2021-02-09 22:04:16 +0100

    did the model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,11 +1,4 @@
 class Booking < ApplicationRecord
-  attr_accessor :id
-
-  def initialize(properties = {})
-    @id = properties[:id]
-    @flat_id = properties[:flat_id]
-    @user_id = properties[:user_id]
-    @start_date = properties[:start_date]
-    @end_date = properties[:end_date]
-  end
+  belongs_to :user
+  belongs_to :flat
 end
