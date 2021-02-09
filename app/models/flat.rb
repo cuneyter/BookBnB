0e5dc51fe99723696d4d16034aa0cc7d75644d6b
Cuneyt Ergun <cuneytergun@gmail.com>
2021-02-09 19:20:38 +0000

    add validations in the flat.rb file

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,10 @@
 class Flat < ApplicationRecord
   # belongs_to :user
+
+  validates :address_line_1, presence: true
+  validates :address_line_2, presence: true
+  validates :postcode, presence: true
+  validates :city, presence: true
+  validates :price, presence: true
+  validates :max_guest_number, presence: true
 end
