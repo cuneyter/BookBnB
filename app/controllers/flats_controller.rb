cd0e0cc836c6fcbafcbc63b73f2b27295470516e
Signe Bunke <signebunke@gmail.com>
2021-02-11 22:43:31 +0100

    created flat controllers files

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,29 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  # def show
+  #   @flat = Flat.find(params[:id])
+  #   @booking = Booking.new
+  # end
+
+  # def new
+  #   @flat = Flat.new
+  # end
+
+  # def create
+  #   @flat = Flat.new(flat_params)
+  #   if @flat.save
+  #     redirect_to flat_path(@flat)
+  #   else
+  #     render 'new'
+  #   end
+  # end
+
+  # private
+
+  # def flat_params
+  #   params.require(:flat).permit(:city, :price)
+  # end
+end
