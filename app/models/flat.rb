5ae454d99844b221428aca45b22d9794941ec112
Cuneyt Ergun <cuneytergun@gmail.com>
2021-02-17 21:08:19 +0000

    add the geocode in the flat.rb

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -5,4 +5,10 @@ class Flat < ApplicationRecord
 
   # validates :address_line_1, :address_line_2, :postcode, :city, presence: true
   # validates :price, :max_guest_number, presence: true, numericality: true
+  geocoded_by :postcode
+  after_validation :geocode, if: :will_save_change_to_postcode?
+
+  # def address
+  #   [address_line_1, city, postcode].compact.join(', ')
+  # end
 end
